66bd7339010b0c2ea5b40e1ed1951d725dad4913
Richard McCormack <brick@brick.codes>
2017-11-18 21:08:53 -0500

    New data models. Plus, can now filter by best things of species. neat

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@
 #![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
-#[macro_use]
+extern crate crawl_model;
 extern crate diesel;
-#[macro_use]
-extern crate diesel_codegen;
 extern crate dotenv;
 extern crate r2d2;
 extern crate r2d2_diesel;
@@ -16,82 +14,164 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-mod schema;
-mod models;
-
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use rocket_contrib::Template;
-use rocket_contrib::Json;
 use dotenv::dotenv;
-use rocket::request::Form;
-use rocket::http::{Cookie, Cookies, Status};
 use rocket::response::NamedFile;
 use rocket::State;
-use std::collections::HashMap;
 use std::path::{Path, PathBuf};
-use diesel::query_builder::AsChangeset;
-use rocket::request::{self, Request, FromRequest};
-use rocket::Outcome;
+use std::ops::Deref;
 
 type DatabasePool = r2d2::Pool<r2d2_diesel::ConnectionManager<diesel::SqliteConnection>>;
 
+struct Species(crawl_model::data::Species);
+
+impl Deref for Species {
+    type Target = crawl_model::data::Species;
+    fn deref(&self) -> &crawl_model::data::Species {
+        &self.0
+    }
+}
+
+impl<'a> rocket::request::FromParam<'a> for Species {
+    type Error = ();
+    fn from_param(param: &'a rocket::http::RawStr) -> Result<Species, ()> {
+        let species = param.percent_decode_lossy().parse::<crawl_model::data::Species>()?;
+        Ok(Species(species))
+    }
+}
+
+struct Background(crawl_model::data::Background);
+
+impl Deref for Background {
+    type Target = crawl_model::data::Background;
+    fn deref(&self) -> &crawl_model::data::Background {
+        &self.0
+    }
+}
+
+impl<'a> rocket::request::FromParam<'a> for Background {
+    type Error = ();
+    fn from_param(param: &'a rocket::http::RawStr) -> Result<Background, ()> {
+        let bg = param.percent_decode_lossy().parse::<crawl_model::data::Background>()?;
+        Ok(Background(bg))
+    }
+}
+
+
 #[derive(Serialize)]
 struct IndexContext {
-    morgues: Vec<FormattedMorgue>,
+    games: Vec<FormattedGame>,
 }
 
 #[derive(Serialize)]
-struct FormattedMorgue {
+struct FormattedGame {
     pub real_name: String,
     pub name: String,
     pub score: i64,
-    pub race: String,
+    pub species: String,
     pub background: String,
 }
 
-impl From<models::DbMorgue> for FormattedMorgue {
-    fn from(morgue: models::DbMorgue) -> FormattedMorgue {
-        let race = unsafe { std::mem::transmute::<i64, Race>(morgue.race) };
-        let background = unsafe { std::mem::transmute::<i64, Background>(morgue.background) };
-        let real_name = match morgue.name.as_ref() {
+impl From<crawl_model::db_model::Game> for FormattedGame {
+    fn from(game: crawl_model::db_model::Game) -> FormattedGame {
+        let species = unsafe { std::mem::transmute::<i64, crawl_model::data::Species>(game.species_id) };
+        let background = unsafe { std::mem::transmute::<i64, crawl_model::data::Background>(game.background_id) };
+        let real_name = match game.name.as_ref() {
             "brick" => "Richard",
             "Peen" => "Paul",
             "max"|"PunishedMax" => "Max",
             "daddy"|"fuckboy3000" => "James",
             "sweetBro" => "Luca",
             "hellaJeff" => "Ben H",
-            "Richard" => "Ben S",
+            "Richard"|"BoonShekel" => "Ben S",
             _ => "?"
         };
-        FormattedMorgue {
-            name: morgue.name,
+        FormattedGame {
+            name: game.name,
             real_name: String::from(real_name),
-            score: morgue.score,
-            race: format!("{:?}", race),
+            score: game.score,
+            species: format!("{:?}", species),
             background: format!("{:?}", background)
         }
     }
 }
 
+#[get("/<species>")]
+fn best_species(state: State<DatabasePool>, species: Species) -> Template {
+    let connection = state.get().expect("Timeout waiting for pooled connection");
+    let games = {
+        use crawl_model::db_schema::games::dsl::*;
+        games
+            .filter(species_id.eq(*species as i64))
+            .order(score.desc())
+            .limit(100)
+            .load::<crawl_model::db_model::Game>(&*connection)
+            .expect("Error loading games")
+    };
+    let formatted_games = games.into_iter().map(|x| x.into() ).collect();
+    let context = IndexContext { games: formatted_games };
+    Template::render("index", &context)
+}
+
+#[get("/<bg>", rank = 2)]
+fn best_bg(state: State<DatabasePool>, bg: Background) -> Template {
+    let connection = state.get().expect("Timeout waiting for pooled connection");
+    let games = {
+        use crawl_model::db_schema::games::dsl::*;
+        games
+            .filter(background_id.eq(*bg as i64))
+            .order(score.desc())
+            .limit(100)
+            .load::<crawl_model::db_model::Game>(&*connection)
+            .expect("Error loading games")
+    };
+    let formatted_games = games.into_iter().map(|x| x.into() ).collect();
+    let context = IndexContext { games: formatted_games };
+    Template::render("index", &context)
+}
+
+#[get("/<species>/<bg>")]
+fn best_combo(state: State<DatabasePool>, species: Species, bg: Background) -> Template {
+    let connection = state.get().expect("Timeout waiting for pooled connection");
+    let games = {
+        use crawl_model::db_schema::games::dsl::*;
+        games
+            .filter(species_id.eq(*species as i64))
+            .filter(background_id.eq(*bg as i64))
+            .order(score.desc())
+            .limit(100)
+            .load::<crawl_model::db_model::Game>(&*connection)
+            .expect("Error loading games")
+    };
+    let formatted_games = games.into_iter().map(|x| x.into() ).collect();
+    let context = IndexContext { games: formatted_games };
+    Template::render("index", &context)
+}
+
+#[get("/<bg>/<species>", rank = 2)]
+fn best_combo_inverted(state: State<DatabasePool>, species: Species, bg: Background) -> Template {
+    best_combo(state, species, bg)
+}
 
 #[get("/")]
 fn hiscores(state: State<DatabasePool>) -> Template {
     let connection = state.get().expect("Timeout waiting for pooled connection");
-    let mut morgues = {
-        use schema::morgues::dsl::*;
-        morgues
+    let games = {
+        use crawl_model::db_schema::games::dsl::*;
+        games
             .order(score.desc())
             .limit(100)
-            .load::<models::DbMorgue>(&*connection)
-            .expect("Error loading morgues")
+            .load::<crawl_model::db_model::Game>(&*connection)
+            .expect("Error loading games")
     };
-    let formatted_morgues = morgues.into_iter().map(|x| x.into() ).collect();
-    let context = IndexContext { morgues: formatted_morgues };
+    let formatted_games = games.into_iter().map(|x| x.into() ).collect();
+    let context = IndexContext { games: formatted_games };
     Template::render("index", &context)
 }
 
-#[get("/<file..>", rank = 2)]
+#[get("/<file..>", rank = 3)]
 fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
@@ -104,79 +184,8 @@ fn main() {
     let manager = r2d2_diesel::ConnectionManager::<SqliteConnection>::new(database_url);
     let pool = r2d2::Pool::new(config, manager).expect("Failed to create pool.");
     rocket::ignite()
-        .mount("/", routes![hiscores, files])
+        .mount("/", routes![hiscores, files, best_species, best_bg, best_combo, best_combo_inverted])
         .manage(pool)
         .attach(Template::fairing())
         .launch();
-}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(i64)]
-enum Race {
-    Barachi = 0,
-    Centaur,
-    DeepDwarf,
-    DeepElf,
-    Demigod,
-    Demonspawn,
-    Draconian,
-    RedDraconian,
-    WhiteDraconian,
-    GreenDraconian,
-    YellowDraconian,
-    GreyDraconian,
-    BlackDraconian,
-    PurpleDraconian,
-    MottledDraconian,
-    PaleDraconian,
-    Felid,
-    Formicid,
-    Gargoyle,
-    Ghoul,
-    Gnoll,
-    Halfling,
-    HighElf,
-    HillOrc,
-    Human,
-    Kobold,
-    Merfolk,
-    Minotaur,
-    Mummy,
-    Naga,
-    Ocotopode,
-    Ogre,
-    Spriggan,
-    Tengu,
-    Troll,
-    Vampire,
-    VineStalker,
-}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(i64)]
-enum Background {
-    Fighter = 0,
-    Gladiator,
-    Monk,
-    Hunter,
-    Assassin,
-    Berserker,
-    AbyssalKnight,
-    ChaosKnight,
-    Skald,
-    Enchanter,
-    Transmuter,
-    ArcaneMarksman,
-    Warper,
-    Wizard,
-    Conjurer,
-    Summoner,
-    Necromancer,
-    FireElementalist,
-    IceElementalist,
-    AirElementalist,
-    EarthElementalist,
-    VenomMage,
-    Artificer,
-    Wanderer,
 }
